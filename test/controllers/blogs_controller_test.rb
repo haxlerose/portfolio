4fae8436e1985d24745e2f5b1ef5e52b1a4a5517
Paul Chesterton <chestertonpa@gmail.com>
2026-05-16 11:38:57 -0400

    Fix navbar section links from blog pages

--- test/controllers/blogs_controller_test.rb
+++ test/controllers/blogs_controller_test.rb
@@ -6,6 +6,16 @@ class BlogsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "navbar section links point back to home page anchors" do
+    get blogs_url
+    assert_response :success
+
+    assert_select "a[href='#{root_path(anchor: "about-me")}']", text: "About"
+    assert_select "a[href='#{root_path(anchor: "skills")}']", text: "Skills"
+    assert_select "a[href='#{root_path(anchor: "projects")}']", text: "Projects"
+    assert_select "a[href='#{root_path(anchor: "contact")}']", text: "Contact"
+  end
+
   test "should return 404 for invalid slug" do
     get blog_post_url(slug: "nonexistent-post")
     assert_response :not_found
